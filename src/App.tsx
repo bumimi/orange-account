d3fd54a26f40765f602bac87a6d0773267b71e52
bumimi <yanouyang12@163.com>
2024-07-10 08:38:22 +0000

    引入vue-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { defineComponent, ref } from "vue"
-
+import {RouterLink, RouterView} from "vue-router"
 export const App = defineComponent({
     setup(){
-        //声明函数和变量
-        const count = ref(0)
-        const onClick = () =>{
-            count.value +=1
-        }
+        
         //返回标签
         return () => (<>
-        <div>{count.value}</div>
-        <button onClick={onClick}>+1</button>
+        <RouterView />
+        <RouterLink to="/">Go to Foo</RouterLink>
+        <RouterLink to="/about">Go to Bar</RouterLink>
         
         </>
         )
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import {App} from "./App"
+import {  createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import {Foo} from '../views/Foo'
+import {Bar} from '../views/Bar'
+
+const routes = [
+  { path: '/', component: Foo },
+  { path: '/about', component: Bar },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+createApp(App).use(router).mount('#app')
