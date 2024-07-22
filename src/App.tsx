a263d456ec1edb0523185bb15ca987788d3986fc
bumimi <yanouyang12@163.com>
2024-07-22 08:25:55 +0000

    创建欢迎界面

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { defineComponent, ref } from "vue"
-import {RouterLink, RouterView} from "vue-router"
+import { RouterLink, RouterView } from "vue-router"
 export const App = defineComponent({
-    setup(){
-        
+    setup() {
+
         //返回标签
         return () => (<>
-        <RouterView />
-        <RouterLink to="/">Go to Foo</RouterLink>
-        <RouterLink to="/about">Go to Bar</RouterLink>
-        
+            <header>导航
+                <ul>
+                    <li>
+                        <RouterLink to="/">Go to Foo</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/about">Go to Bar</RouterLink>
+                    </li>
+                </ul>
+            </header>
+            <div>
+                <RouterView />
+            </div>
+            <footer>页脚</footer>
         </>
         )
     }
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,24 @@
 import { Foo } from '../views/Foo'
 import {Bar} from '../views/Bar'
+import { RouteRecordRaw } from 'vue-router'
+import { Welcome } from '../views/Welcome'
+import { First } from '../components/Welcome/First'
+import { Second } from '../components/Welcome/Second'
+import { Third } from '../components/Welcome/Third'
+import { Fourth } from '../components/Welcome/Fourth'
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/about', component: Bar },
+  { 
+    path: '/welcome',
+    component:Welcome, 
+    children:[
+      {path: '1', component: First},
+      {path: '2', component: Second},
+      {path: '3', component: Third},
+      {path: '4', component: Fourth},
+
+    ] },
+
 ]
